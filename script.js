9a1bca0a4ca198f49550933e344f3449599b0412
José Lucena <kleitonjose256@gmail.com>
2024-03-03 02:40:57 -0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-var elementoDuvidas = document.querySelectorAll(".duvida");
 
-elementoDuvidas.forEach(function (duvida) {
-    duvida.addEventListener('click', function () {
-        duvida.classList.toggle('ativa');
-    });
-});
 
